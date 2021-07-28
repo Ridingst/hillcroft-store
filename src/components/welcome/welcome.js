78da305f4d2d23dabbb4b8c94576f2f8e92dd7ba
Thomas Ridings <thomas.a.ridings@gmail.com>
2021-07-28 23:09:27 +0100

    pushing api test to vercel

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -11,5 +11,5 @@ For our use case, we only care about the name, not the other information.
 Here, we'll create a derived store to hold the name.
 **/
 export const person = derived(apiData, ($apiData) => {
-	return { name: $apiData.name || "World" }
+	return { name: $apiData.name || "There" }
 });
