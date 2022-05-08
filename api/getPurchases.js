2756cbaceafe915f1d05c4a0e4c72ca41de958b7
Thomas Ridings <thomas.a.ridings@gmail.com>
2022-05-08 22:01:14 +0100

    adding auth

--- api/getPurchases.js
+++ api/getPurchases.js
@@ -1,3 +1,4 @@
+const { read } = require('fs');
 const { resolve } = require('path');
 const Stripe = require('stripe');
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
@@ -20,31 +21,38 @@ async function getInvoices(){
 };
 
 module.exports = (req, res) => {
-    const { body } = req;    
+    const { body } = req; 
+    if(req.query.apiPassword == process.env.apiPassword){
+        getInvoices()
+        .then(data =>{
+            return _.map(data, function(item){
+                return {
+                    customer: item.customer,
+                    customer_email: item.customer_email,
+                    customer_name: item.customer_name,
+                    amount_paid: item.amount_due,
+                    customer_phone: item.customer_phone,
+                    product: item.lines.data[0].description,
+                    date: item.created
+                }
+            })
+        })
+        .then(data => 
+            res.send(data)
+        )
+        .catch(err => {
+            console.error((err))
+            res.status(400)
+            res.send({
+                status: 'Error',
+                message: "Error triggering event"
+            })
+        })
 
-    getInvoices()
-    .then(data =>{
-        return _.map(data, function(item){
-            return {
-                customer: item.customer,
-                customer_email: item.customer_email,
-                customer_name: item.customer_name,
-                amount_paid: item.amount_due,
-                customer_phone: item.customer_phone,
-                product: item.lines.data[0].description,
-                date: item.created
-            }
-        })
-    })
-    .then(data => 
-        res.send(data)
-    )
-    .catch(err => {
-        console.error((err))
-        res.status(400)
-        res.send({
-            status: 'Error',
-            message: "Error triggering event"
-        })
-    })
+    } else {
+        res.status(401);
+        res.send("Not Authorized")
+    }
+
+    
 } 
